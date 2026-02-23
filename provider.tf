b3cfdbe5ccdbaa7295ec34b75ed2207bd2042677
Vijay <isaradhi24@gmail.com>
2026-02-22 23:46:03 -0500

    add path variable in provider for cross-platforms

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "us-east-2"
-  shared_credentials_files = ["C:/Users/isara/.aws/credentials"]
+  # shared_credentials_files = ["C:/Users/isara/.aws/credentials"]
+  shared_credentials_files = ["${pathexpand("~/.aws/credentials")}"]
 
 }
